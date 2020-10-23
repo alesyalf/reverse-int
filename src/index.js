99d9f01e7dc834092fd9886db023b761bcf52d36
Alesya Demeshko <demeshko.ab@phystech.edu>
2020-10-23 22:36:29 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let k = 0;
+  let n_copy = n;
+  while (n_copy){
+      n_copy = Math.floor(n_copy / 10);
+      k += 1;
+  }
+  let answer = 0;
+  for (let i = k - 1; i >= 0; --i){
+      answer += (n % 10) * Math.pow(10, i);
+      n = Math.floor(n / 10);
+  }
+  return answer;
 }
